11e90928a6a85c17c259bd28f2d043b28eb04ac2
Muhammad <muhammadbinimran407@gmail.com>
2026-02-19 23:14:09 +0500

    Auth ha taky arror naaye bna diya ha

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/teacher", function () {
-    return "Teacher Page";
-})->name("teacher.dashboard");
 
-Route::get("/student", function () {
-    return "Student Page";
-})->name("student.dashboard");
-Route::get('/admin', function () {
-    return "Admin Page";
-})->middleware(['auth', 'verified'])->name('admin.dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -23,16 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
-
-
-/**
-* Sheraz will create all the layouts for three user types
-* Talha will redesign the auth pages (login and register)
-* Zain will seed 10 courses with factories and seeder , must exist in db
-* Abdullah will creat the index and show pages for courses
-* Hassan will create course pages (index , show and create) in the admin panel
-* Imran will create the index and show pages for user panel
-
-
-*/
+require __DIR__.'/auth.php';
